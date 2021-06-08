86c89cc740790697dbc67df07a4d44ca9f03e6e2
Karan Rathod <karanrathod@Karans-MacBook-Air-2.local>
2021-06-08 23:25:08 +0530

    initial commit

---
+++ controllers/blogsRouter.js
@@ -0,0 +1,69 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (req, res) => {
+  Blog.find({}).then((blogs) => {
+    res.json(blogs);
+  });
+});
+
+blogsRouter.get("/:id", (req, res, next) => {
+  Blog.findById(req.params.id)
+    .then((blog) => {
+      if (blog) {
+        res.json(blog);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+blogsRouter.post("/", (req, res, next) => {
+  const body = req.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  blog
+    .save()
+    .then((savedNote) => {
+      res.json(savedNote);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+blogsRouter.delete("/:id", (req, res, next) => {
+  Blog.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogsRouter;
