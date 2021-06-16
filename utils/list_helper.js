9c578fcb9f7b36303b603c9b2d6b608a103ae24a
Karan Rathod <karanrathod@Karans-MacBook-Air-2.local>
2021-06-16 13:42:17 +0530

    add more dummy tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,4 +3,23 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+let totalLikes = (blogs) => {
+  let total;
+  if (blogs.length < 1) {
+    total = 0;
+  } else {
+    blogs.reduce((acc, val) => {
+      acc += val.likes;
+      total = acc;
+      return acc;
+    }, 0);
+  }
+  return total;
+};
+
+const favoriteBlog = (blogs) => {
+  let sortedBlogs = blogs.sort((a, b) => b.likes - a.likes);
+  let result = sortedBlogs[0];
+  return result;
+};
+module.exports = { dummy, totalLikes, favoriteBlog };
